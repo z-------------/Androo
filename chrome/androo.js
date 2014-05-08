b971e11b870f327eeedb63e87f2b0107fd440b11
z------------- <zacharyguard@gmail.com>
2014-05-08 21:29:15 +0800

    Added some phrases and lowered pitch
    For a more authentic Androo experience

--- chrome/androo.js
+++ chrome/androo.js
@@ -1,4 +1,4 @@
-var phrases = ["Were you born in a barn?", "Get some work done", "Useless scrub", "Stop jacking off", "I'll bust your chops", "Are you born in a water park?", "Stop fapping around", "Are you born in a barn?", "Later, shizlord", "Oy!"];
+var phrases = ["Were you born in a barn?", "Get some work done.", "Useless scrub.", "Stop jacking off.", "I'll bust your chops.", "Are you born in a water park?", "Stop fapping around.", "Are you born in a barn?", "Later, shizlord.", "Oy!"];
 var dialogs = ["WERE U BORN IN A BARN???", "GET SUM WERK DUN", "Useless scrub...", "Stop jacking off", "I'LL BUST YOUR CHOPS", "R U BORN IN A WATER PARK??", "Stop fapping around", "R U BORN IN A BARN???", "LATER, SHIZLORD", "OI!!"];
 
 function androo(){
@@ -6,7 +6,8 @@ function androo(){
     
     var ttsOptions = {
         gender: "male",
-        rate: 0.7
+        rate: 0.7,
+        pitch: 0.8
     }
     
     chrome.tts.speak(phrases[index],ttsOptions);
