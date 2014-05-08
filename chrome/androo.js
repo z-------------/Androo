f87c8c02b6b5191bb655d2a771093ffde0325a13
Zachary Guard <zacharyguard@gmail.com>
2014-05-08 17:44:03 +0800

    Added delay randomisation

--- chrome/androo.js
+++ chrome/androo.js
@@ -1,7 +1,7 @@
 var phrases = ["Were you born in a barn?", "Get some work done", "Useless scrub", "Stop jacking off", "I'll bust your chops", "Are you born in a water park?", "Stop fapping around", "Are you born in a barn?", "Later, shizlord", "Oy!"];
 var dialogs = ["WERE U BORN IN A BARN???", "GET SUM WERK DUN", "Useless scrub...", "Stop jacking off", "I'LL BUST YOUR CHOPS", "R U BORN IN A WATER PARK??", "Stop fapping around", "R U BORN IN A BARN???", "LATER, SHIZLORD", "OI!!"];
 
-function doAlert(){
+function androo(){
     var index = Math.round(Math.random()*(phrases.length-1));
     
     var ttsOptions = {
@@ -12,6 +12,8 @@ function doAlert(){
     
     chrome.tts.speak(phrases[index],ttsOptions);
     alert(dialogs[index]);
+    
+    setTimeout(androo,Math.random()*60*1000);
 }
 
-setInterval(doAlert,20000);
+setTimeout(androo,20000);
