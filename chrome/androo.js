d84c4135bf4ccca30ebebf8b7a8920a7d63a3d7b
Zachary Guard <zacharyguard@gmail.com>
2014-05-09 12:25:52 +0800

    Fixed TTS

--- chrome/androo.js
+++ chrome/androo.js
@@ -1,6 +1,6 @@
 var phrases = ["Were you born in a barn?", "Get some work done.", "Useless scrub.", "Stop jacking off.", "I'll bust your chops.", "Are you born in a water park?", "Stop fapping around.", "Are you born in a barn?", "Later, shizlord.", "Oy!", "Urh, scrub!", "Excuse me?"];
 var dialogs = ["WERE U BORN IN A BARN???", "GET SUM WERK DUN", "Useless scrub...", "Stop jacking off", "I'LL BUST YOUR CHOPS", "R U BORN IN A WATER PARK??", "Stop fapping around", "R U BORN IN A BARN???", "LATER, SHIZLORD", "OI!!", "URGHHHH SCRUB!!!", "ESCUUUUSE MEEEE"];
-var remarks = ["Scrub", "Useless twerp", "Uhh Uhh Uhh", "Uhh"];
+var remarks = ["Scrub", "Useless twerp", "Uh Uh Uh", "Uh"];
 
 function androo(){
     var index = Math.round(Math.random()*(phrases.length-1));
@@ -9,12 +9,13 @@ function androo(){
     var ttsOptions = {
         gender: "male",
         rate: 0.7,
-        pitch: 0.8
+        pitch: 0.8,
+        enqueue: true
     }
     
     chrome.tts.speak(phrases[index],ttsOptions);
     alert(dialogs[index]);
-    chrome.tts.speak(remarks[rIndex],{enqueue:true});
+    chrome.tts.speak(remarks[rIndex],ttsOptions);
     
     setTimeout(androo,Math.random()*30*1000);
 }
