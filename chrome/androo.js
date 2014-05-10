0628e1b3680f5e26d5f706e04e1130b330b0b364
Zachary Guard <zacharyguard@gmail.com>
2014-05-10 16:32:10 +0800

    Fixed alignment of phrases

--- chrome/androo.js
+++ chrome/androo.js
@@ -1,5 +1,5 @@
 var phrases = ["Were you born in a barn?", "Get some work done.", "Useless scrub.", "Stop jacking off.", "I'll bust your chops.", "Are you born in a water park?", "Stop fapping around.", "Are you born in a barn?", "Later, shizlord.", "Oy!", "Urh, scrub!", "Excuse me?", "You should know when to give up...", "You're so pathetic", "How... will this help your future career?","Ugh Ug Uh Guh Gug Guh Gugh Huh","Stop smirking."];
-var dialogs = ["WERE U BORN IN A BARN???", "GET SUM WERK DUN", "Useless scrub...", "Stop jacking off", "I'LL BUST YOUR CHOPS", "R U BORN IN A WATER PARK??", "Stop fapping around", "R U BORN IN A BARN???", "LATER, SHIZLORD", "OI!!", "URGHHHH SCRUB!!!", "ESCUUUUSE MEEEE", "You should know when to give up...", "How will this help your future career?", "UR SO PATHETIC", "UGH UG UH GUH GUG GUH GUGH HUH", "STOP SMIRKING."];
+var dialogs = ["WERE U BORN IN A BARN???", "GET SUM WERK DUN", "Useless scrub...", "Stop jacking off", "I'LL BUST YOUR CHOPS", "R U BORN IN A WATER PARK??", "Stop fapping around", "R U BORN IN A BARN???", "LATER, SHIZLORD", "OI!!", "URGHHHH SCRUB!!!", "ESCUUUUSE MEEEE", "You should know when to give up...", "UR SO PATHETIC", "How will this help your future career?", "UGH UG UH GUH GUG GUH GUGH HUH", "STOP SMIRKING."];
 var remarks = ["Scrub", "Useless twerp", "Uh Uh Uh", "Uh"];
 
 function androo(){
